a85b55b894c40acff72670a48ee8ab4a732f509a
soloiaros <soloiaros@gmail.com>
2025-07-04 12:32:51 -0700

    Initial commit

---
+++ GoodReadDaily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodReadDaily
+//
+//  Created by Yaroslav Solovev on 7/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
