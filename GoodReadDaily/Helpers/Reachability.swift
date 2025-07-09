1310f82daf581ca805b0a9403ef6088099a70f9d
soloiaros <soloiaros@gmail.com>
2025-07-09 16:27:11 -0700

    Add external API for words and new Word Controller this commit adds a separate view controller for the action of tapping a word in the dictionary. It uses an external free API for fetching all the necessary information. In case of connection issues, a corresponding label is shown, as well as in the case of entering a word that is not in the database.

---
+++ GoodReadDaily/Helpers/Reachability.swift
@@ -0,0 +1,34 @@
+//
+//  Reachability.swift
+//  GoodReadDaily
+//
+//  Created by Yaroslav Solovev on 7/9/25.
+//
+
+import SystemConfiguration
+
+class Reachability {
+    static func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+}
